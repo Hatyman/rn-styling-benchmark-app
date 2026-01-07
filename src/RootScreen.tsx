184cab11fe11e805750939746c65ed8db1a3db5d
Kirill Tiunderov <kty@outsi.de>
2026-01-07 23:58:18 +0900

    added meastro draft test

--- src/RootScreen.tsx
+++ src/RootScreen.tsx
@@ -3,6 +3,11 @@ import { LargeListCase } from '@/cases/largeList/LargeListCase.tsx';
 import { TabBar } from '@/components/TabBar.tsx';
 import { LargeScrollCase } from '@/cases/largeScroll/LargeScrollCase.tsx';
 
+// com.stylingbenchmarkapp
+// flashlight test --bundleId com.stylingbenchmarkapp --testCommand "adb shell input swipe 300 800 300 100 50" --duration 10000 --resultsFilePath flashlight/reports/nude.json --resultsTitle "Nude" --iterationCount 3
+// flashlight test --bundleId com.stylingbenchmarkapp --testCommand "npx @perf-profiler/maestro@latest test flashlight/large-list.yml" --duration 10000 --resultsFilePath flashlight/reports/nude.json --resultsTitle "Nude" --iterationCount 3
+// flashlight test --bundleId com.stylingbenchmarkapp --testCommand "maestro test flashlight/large-list.yml" --duration 60000 --resultsFilePath flashlight/reports/nude.json --resultsTitle "Nude" --iterationCount 3
+
 export const RootScreen: FC = function RootScreen() {
   const [mode, setMode] = useState<'list' | 'scroll'>('list');
 
